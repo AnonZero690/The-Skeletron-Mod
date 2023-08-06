397437b28065e2e20406d17c140a37a202b4b33d
AnonZero960 <NullZero1@outlook.com>
2023-08-07 00:30:32 +0100

    Localization, Damageclass and Mod Icon

---
+++ Common/DamageClass/Bonecurseed.cs
@@ -0,0 +1,25 @@
+﻿
+using Terraria;
+using Terraria.ModLoader;
+
+public class Bonecursed : DamageClass
+{
+    public override StatInheritanceData GetModifierInheritance(DamageClass damageClass)
+    {
+        if (damageClass == DamageClass.Generic)
+            return StatInheritanceData.Full;
+
+        return new StatInheritanceData(
+            damageInheritance: 0f,
+            critChanceInheritance: 0f,
+            attackSpeedInheritance: 0f,
+            knockbackInheritance: 0f);
+    }
+    public override void SetDefaultStats(Player player)
+    {
+        player.GetAttackSpeed<Bonecursed>() += 4;
+        player.GetArmorPenetration<Bonecursed>() += 0.5f;
+        player.GetDamage<Bonecursed>() += 0.2f;
+    }
+    public override bool UseStandardCritCalcs => true;
+}
