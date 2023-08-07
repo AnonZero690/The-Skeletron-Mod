8e9cb03f0600de87c638a67f759d1158de1642ac
ChadBroski01 <aalexx822@gmail.com>
2023-08-07 08:28:58 -0400

    Fixed a bug where the Sharpened Bone Projectile was in the wrong Namespace, and fixed a bug where the BoneDagger and Sharpened Bone were using the wrong Namespace

--- Items/Weapons/Melee/BoneDagger.cs
+++ Items/Weapons/Melee/BoneDagger.cs
@@ -3,7 +3,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 using Terraria.DataStructures;
-using TheSkeletronMod.projectiles;
+using TheSkeletronMod.Projectiles;
 using TheSkeletronMod.Common.DamageClasses;
 
 namespace TheSkeletronMod.Items.Weapons.Melee
