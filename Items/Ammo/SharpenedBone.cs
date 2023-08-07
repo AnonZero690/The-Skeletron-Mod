d88b52547faea5c926b8bac6406d9a550c1a6f5d
ChadBroski01 <aalexx822@gmail.com>
2023-08-07 08:28:58 -0400

    Changed all instances of "projectile" to "Projectile" when referencing the folder

--- Items/Ammo/SharpenedBone.cs
+++ Items/Ammo/SharpenedBone.cs
@@ -2,7 +2,7 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.GameContent.Creative;
-using TheSkeletronMod.projectiles;
+using TheSkeletronMod.Projectiles;
 using TheSkeletronMod.Common.DamageClasses;
 
 namespace TheSkeletronMod.Items.Ammo
