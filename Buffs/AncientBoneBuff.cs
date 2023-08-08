3ed5517720d4d0448527a7c04d139be66dbf4d1f
lunispang <95270420+lunispang@users.noreply.github.com>
2023-08-08 23:46:31 +0200

    Ancient Bone armor set bonus update

--- Buffs/AncientBoneBuff.cs
+++ Buffs/AncientBoneBuff.cs
@@ -15,16 +15,22 @@ namespace TheSkeletronMod.Buffs
         public override LocalizedText Description => base.Description;
         public override void Update(Player player, ref int buffIndex)
         {
+            double dist = 200;
             for (int i = 0; i < 90; i++) // I could do 360 here, instead of 90 (and then remove the "* 4" from the deg variable), and it still works well
             {
                 double deg = i * 4;
-                double rad = (deg) * (3.1415 / 180);
-                double dist = 200;
-
-                float x2 = player.position.X - (float)(Math.Cos(rad) * dist);
-                float y2 = player.position.Y - (float)(Math.Sin(rad) * dist);
-                int dust = Dust.NewDust(new Vector2(x2 - player.velocity.X, y2 - player.velocity.Y), 1, 1, DustID.Bone, 0f, 0f, 0, default(Color), 1f);
+                double rad = (deg) * (Math.PI / 180);
+                float x2 = player.Center.X - (float)(Math.Cos(rad) * dist);
+                float y2 = player.Center.Y - (float)(Math.Sin(rad) * dist);
+                int dust = Dust.NewDust(new Vector2(x2, y2), 3, 3, DustID.Stone, 0f, 0f, 0, new Color(175, 160, 120), 1f);
                 Main.dust[dust].noGravity = true;
+            }
+            for (int i = 0; i<200; i++){
+                if (((Main.npc[i].Center.X-player.Center.X)*(Main.npc[i].Center.X-player.Center.X))+((Main.npc[i].Center.Y-player.Center.Y)*(Main.npc[i].Center.Y-player.Center.Y))<(dist*dist)){
+                    if(!Main.npc[i].friendly){
+                        Main.npc[i].AddBuff(ModContent.BuffType<BonedDebuff>(),180);
+                    }
+                }
             }
         }
     }
