47629b1b03abb9953427577bc5c93462911ded0e
AnonZero960 <NullZero1@outlook.com>
2023-08-11 15:20:07 +0100

    hi

---
+++ Modplayer/SkullModplayer.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ModLoader;
+using TheSkeletronMod.Items.Accessories;
+
+namespace TheSkeletronMod.Modplayer
+{
+    public class SkullModplayer : ModPlayer
+    {
+        public override IEnumerable<Item> AddStartingItems(bool mediumCoreDeath)
+        {
+
+            return (IEnumerable<Item>)(object)new Item[1]
+            {
+                new Item(ModContent.ItemType<SkullPendant>(), 1, 0),
+            };
+        }
+    }
+}
