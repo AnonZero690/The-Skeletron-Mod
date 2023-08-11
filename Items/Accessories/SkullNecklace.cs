ce74e413559571e14be6dd8801f520e36eecb3b2
ChadBroski01 <aalexx822@gmail.com>
2023-08-11 11:17:53 -0400

    Skull necklace go brrr

--- Items/Accessories/SkullNecklace.cs
+++ Items/Accessories/SkullNecklace.cs
@@ -6,10 +6,12 @@ using Microsoft.Xna.Framework;
 using TheSkeletronMod.Items.Materials;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
+using TheSkeletronMod.Items.Accessories;
+using TheSkeletronMod.Common.DamageClasses;
 
 namespace TheSkeletronMod.Items.Accessories
 {
-    internal class SkullPendant : ModItem
+    internal class SkullNecklace : ModItem
     {
         public override string Texture => SkeletronUtils.GetVanillaTexture<Item>(ItemID.BandofRegeneration);
         public override void SetDefaults()
@@ -18,11 +20,15 @@ namespace TheSkeletronMod.Items.Accessories
             Item.rare = 1;
             Item.accessory = true;
         }
-
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetDamage(ModContent.GetInstance<Bonecursed>()) += 0.05f;
+            player.endurance += 0.05f;  
+        }
         public override void AddRecipes()
         {
             Recipe recipe = CreateRecipe();
-            recipe.AddIngredient(ModContent.ItemType<AncientBone>(), 5);
+            recipe.AddIngredient(ModContent.ItemType<SkullPendant>(), 1);
             recipe.AddTile(ModContent.TileType<BoneAltar>());
             recipe.Register();
         }
