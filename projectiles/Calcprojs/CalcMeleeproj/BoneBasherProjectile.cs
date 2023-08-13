416814002aa1032f35cd554dbe7a7f620205f450
ChadBroski01 <aalexx822@gmail.com>
2023-08-13 13:11:10 -0400

    KIND OF got the bone basher to work

--- projectiles/Calcprojs/CalcMeleeproj/BoneBasherProjectile.cs
+++ projectiles/Calcprojs/CalcMeleeproj/BoneBasherProjectile.cs
@@ -20,17 +20,29 @@ namespace TheSkeletronMod.projectiles.Calcprojs.CalcMeleeproj
         {
             Projectile.width = 32;
             Projectile.height = 64;
-            Projectile.gfxOffY = (float)100.2;
+            //Projectile.gfxOffY = (float)-100.2;
         }
        public override void AI()
        {
            Player player = Main.LocalPlayer;
-           Vector2 offset = new Vector2(-10,0);
-           float shootToX = Main.mouseX - Projectile.Center.X;
-           float shootToY = Main.mouseY - Projectile.Center.Y;
-           Vector2 d = (Main.MouseWorld - player.position).SafeNormalize(Vector2.UnitX);
+           Vector2 offset = new Vector2(-35,1);
+            if (Main.mouseX > 800)
+            {
+                offset.X += 28;
+            }else
+            {
+                //offset.X += -28;
+            }
+           //float shootToX = Main.mouseX - Projectile.Center.X;
+           //float shootToY = Main.mouseY - Projectile.Center.Y;
+           offset.Y += (Main.mouseY - 300) / 10;
+           Vector2 d = (Main.MouseWorld - player.position).SafeNormalize(Vector2.UnitX).SafeNormalize(Vector2.UnitY);
            Projectile.position = new(player.position.X + offset.X, player.position.Y + offset.Y);
            Projectile.rotation = d.ToRotation() + MathHelper.ToRadians(23);
        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.HealEffect(damageDone * -1);
+        }
     }
 }
