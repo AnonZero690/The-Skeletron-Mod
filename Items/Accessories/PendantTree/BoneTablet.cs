649b95f63a1d09c873169930f74acd4fb1c9f204
AnonZero960 <NullZero1@outlook.com>
2023-08-14 18:30:06 +0100

    Finished the pendant line. Needs summon functionality

--- Items/Accessories/PendantTree/BoneTablet.cs
+++ Items/Accessories/PendantTree/BoneTablet.cs
@@ -7,30 +7,37 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria;
 using TheSkeletronMod.Tiles;
+using Terraria.DataStructures;
 
 namespace TheSkeletronMod.Items.Accessories.PendantTree
 {
-    public class SkullLocket : ModItem
+    public class BoneTablet : ModItem
     {
-
+        public override void SetStaticDefaults()
+        {
+            Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 14));
+            ItemID.Sets.AnimatesAsSoul[Item.type] = true;
+            ItemID.Sets.ItemIconPulse[Item.type] = true;
+        }
         public override void SetDefaults()
         {
-            Item.value = 17000;
-            Item.rare = ItemRarityID.Orange;
+            Item.value = 60000;
+            Item.rare = ItemRarityID.Yellow;
             Item.accessory = true;
         }
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.GetDamage(ModContent.GetInstance<Bonecursed>()) += 0.10f;
-            player.endurance += 0.05f;
+            player.GetDamage(ModContent.GetInstance<Bonecursed>()) += 0.20f;
+            player.endurance += 0.15f;
             Lighting.AddLight(player.position, r: 0.6f, 0.3f, b: 1f);
         }
         public override void AddRecipes()
         {
             Recipe recipe = CreateRecipe();
-            recipe.AddIngredient(ModContent.ItemType<SkullNecklace>(), 1);
-            recipe.AddIngredient(ModContent.ItemType<AncientBone>(), 50);
-            recipe.AddIngredient(ItemID.BookofSkulls, 1);
+            recipe.AddIngredient(ItemID.SolarTablet, 1);
+            recipe.AddIngredient(ItemID.EyeoftheGolem, 1);
+            recipe.AddIngredient(ItemID.LihzahrdBrick, 100);
+            recipe.AddIngredient(ModContent.ItemType<SkullMetal>(), 1);
             recipe.AddTile(ModContent.TileType<BoneAltar>());
             recipe.Register();
         }
--- Items/Accessories/PendantTree/SkullMetal.cs
+++ Items/Accessories/PendantTree/SkullMetal.cs
@@ -10,27 +10,29 @@ using TheSkeletronMod.Tiles;
 
 namespace TheSkeletronMod.Items.Accessories.PendantTree
 {
-    public class SkullLocket : ModItem
+    internal class SkullMetal : ModItem
     {
 
         public override void SetDefaults()
         {
-            Item.value = 17000;
-            Item.rare = ItemRarityID.Orange;
+            Item.value = 50000;
+            Item.rare = ItemRarityID.Pink;
             Item.accessory = true;
         }
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.GetDamage(ModContent.GetInstance<Bonecursed>()) += 0.10f;
-            player.endurance += 0.05f;
+            player.GetDamage(ModContent.GetInstance<Bonecursed>()) += 0.15f;
+            player.endurance += 0.10f;
             Lighting.AddLight(player.position, r: 0.6f, 0.3f, b: 1f);
         }
         public override void AddRecipes()
         {
             Recipe recipe = CreateRecipe();
-            recipe.AddIngredient(ModContent.ItemType<SkullNecklace>(), 1);
-            recipe.AddIngredient(ModContent.ItemType<AncientBone>(), 50);
-            recipe.AddIngredient(ItemID.BookofSkulls, 1);
+            recipe.AddIngredient(ItemID.SoulofFright, 10);
+            recipe.AddIngredient(ItemID.SoulofSight, 10);
+            recipe.AddIngredient(ItemID.SoulofMight, 10);
+            recipe.AddIngredient(ItemID.HallowedBar, 15);
+            recipe.AddIngredient(ModContent.ItemType<SkullToken>(), 1);
             recipe.AddTile(ModContent.TileType<BoneAltar>());
             recipe.Register();
         }
