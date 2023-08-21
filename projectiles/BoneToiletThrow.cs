c03a4d096c268d0a7d32013794cfafd39be1b76d
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-08-21 14:57:09 +1000

    bone toilet item/tile
    GHOOFY WIP

---
+++ projectiles/BoneToiletThrow.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheSkeletronMod.projectiles
+{
+    public class BoneToiletThrow : ModProjectile
+    {
+        public override string Texture => "TheSkeletronMod/Tiles/BoneToilet";
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 9;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 24;
+            Projectile.height = 32;
+
+            Projectile.aiStyle = 2;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ignoreWater = true;
+            Projectile.light = 1f;
+
+            Projectile.tileCollide = true;
+            Projectile.penetrate = 1;
+            Projectile.timeLeft = 400;
+            Projectile.extraUpdates = 2;
+            Projectile.CloneDefaults(ProjectileID.Shuriken);
+        }
+        public override void AI()
+        {
+            Lighting.AddLight(Projectile.position, r: 0.2f, g: 0.2f, b: 0.2f);
+            Lighting.Brightness(1, 1);
+
+            Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.Bone, Projectile.velocity.X * 0.7f, Projectile.velocity.Y * 0.7f, Scale: 0.8f, Alpha: 120);
+
+        }
+
+        public override void Kill(int timeleft)
+
+        {
+
+
+            for (int i = 0; i < 17; i++)
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Bone, 0f, 0f, 50, default, 2f);
+            Player player = Main.player[Projectile.owner];
+
+           
+
+            Collision.AnyCollision(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Shatter, Projectile.position);
+
+            int dustIndex = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.DungeonPink, 0f, 0f, 100, default, 1f);
+
+            Main.dust[dustIndex].noGravity = false;
+            Main.dust[dustIndex].position = Projectile.Center + new Vector2(0f, (float)(-(float)Projectile.height / 2)).RotatedBy(Projectile.rotation, default) * 1.1f;
+            Main.dust[dustIndex].noLight = false;
+
+            int dustIndex1 = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.DungeonPink, 0f, 0f, 255, default, 3f);
+
+            Main.dust[dustIndex1].noGravity = true;
+            Main.dust[dustIndex1].position = Projectile.Center + new Vector2(0f, (float)(-(float)Projectile.height / 2)).RotatedBy(Projectile.rotation, default) * 1.1f;
+            Main.dust[dustIndex1].noLight = false;
+
+            Gore.NewGore(Projectile.GetSource_Death(), Projectile.Center, Vector2.Zero, Mod.Find<ModGore>("BoneSpinGore1").Type, 1f);
+            Gore.NewGore(Projectile.GetSource_Death(), Projectile.Center, Vector2.Zero, Mod.Find<ModGore>("BoneSpinGore2").Type, 1f);
+            Gore.NewGore(Projectile.GetSource_Death(), Projectile.Center, Vector2.Zero, Mod.Find<ModGore>("BoneSpinGore3").Type, 1f);
+        }
+    }
+}
