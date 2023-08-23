feb5f8f41673d679a109b7b24f731d943652c22e
lunispang <95270420+lunispang@users.noreply.github.com>
2023-08-23 18:10:11 +0200

    Create OstropotosisBuff.cs

---
+++ Buffs/OstropotosisBuff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using TheSkeletronMod.Common.DamageClasses;
+namespace TheSkeletronMod.Buffs
+{
+    class OstropotosisBuff : ModBuff
+    {
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetDamage<Bonecursed> () -= 0.5f;
+            player.statDefense *= 0.5f;
+        }
+    }
+}
