97b0a7c1712a376fffcddb1707c4d26dfd968f1c
ChadBroski01 <aalexx822@gmail.com>
2023-08-25 16:53:47 -0400

    2 guardians mattocks fr

---
+++ Items/Tools/GuardiansPickaxe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace TheSkeletronMod.Items.Tools
+{
+    public class GuardiansPickaxe : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.pick = 100;
+            Item.useAnimation = 15;
+            Item.useTime = 15;
+        }
+    }
+}
