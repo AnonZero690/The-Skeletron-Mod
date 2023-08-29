bd278f2594ea16a0ba7bac73063e96a03503e487
ChadBroski01 <aalexx822@gmail.com>
2023-08-29 17:01:57 -0400

    Made the Bone Scythe

---
+++ Items/Weapons/Calcium/CalcMelee/BoneScythe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheSkeletronMod.projectiles.Calcprojs.CalcMeleeproj;
+
+namespace TheSkeletronMod.Items.Weapons.Calcium.CalcMelee
+{
+    public class BoneScythe : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.ItemDefaultMeleeShootCustomProjectile(54, 33, 50, 7f, 25, 25, ItemUseStyleID.Swing, ModContent.ProjectileType<BoneScytheP>(), 10, true);
+        }
+    }
+}
---
+++ projectiles/Calcprojs/CalcMeleeproj/BoneScytheP.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace TheSkeletronMod.projectiles.Calcprojs.CalcMeleeproj
+{
+    public class BoneScytheP : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 20;
+            Projectile.height = 200;
+            Projectile.timeLeft = 600;
+            Projectile.friendly = true;
+            Projectile.penetrate = 2;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.damage = 1;
+            Projectile.light = 10f;
+        }
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2 - 8;
+        }
+    }
+}
