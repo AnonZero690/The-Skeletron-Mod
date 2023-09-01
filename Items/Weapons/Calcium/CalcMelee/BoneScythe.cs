c4f5a49bfbe332d33e4251689d92693fb66d26ed
Mr-Liquid-Sure <141588242+Mr-Liquid-Sure@users.noreply.github.com>
2023-09-01 13:07:19 +0100

    Bone Scythe and reaper, finalising

--- Items/Weapons/Calcium/CalcMelee/BoneScythe.cs
+++ Items/Weapons/Calcium/CalcMelee/BoneScythe.cs
@@ -7,16 +7,21 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using TheSkeletronMod.Common.DamageClasses;
+using TheSkeletronMod.Common.Globals;
 using TheSkeletronMod.projectiles.Calcprojs.CalcMeleeproj;
+
 
 namespace TheSkeletronMod.Items.Weapons.Calcium.CalcMelee
 {
-    public class BoneScythe : ModItem
+    internal class BoneScythe : ModItem, MeleeWeaponWithImprovedSwing
     {
+        public float swingDegree => 150;
+
         public override void SetDefaults()
         {
             Item.ItemDefaultMeleeShootCustomProjectile(54, 33, 70, 7f, 25, 25, ItemUseStyleID.Swing, ModContent.ProjectileType<BoneScytheP>(), 10, false);
             Item.DamageType = ModContent.GetInstance<Bonecursed>();
+            Item.UseSound = SoundID.Item1;
         }
         public override bool MeleePrefix()
         {
--- Items/Weapons/Calcium/CalcMelee/SkeletalReaper.cs
+++ Items/Weapons/Calcium/CalcMelee/SkeletalReaper.cs
@@ -7,16 +7,19 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using TheSkeletronMod.Common.DamageClasses;
+using TheSkeletronMod.Common.Globals;
 using TheSkeletronMod.projectiles.Calcprojs.CalcMeleeproj;
 
 namespace TheSkeletronMod.Items.Weapons.Calcium.CalcMelee
 {
-    public class SkeletalReaper : ModItem
+    public class SkeletalReaper : ModItem, MeleeWeaponWithImprovedSwing
     {
+        public float swingDegree => 170;
         public override void SetDefaults()
         {
             Item.ItemDefaultMeleeShootCustomProjectile(54, 33, 130, 7f, 25, 25, ItemUseStyleID.Swing, ModContent.ProjectileType<SkeletalReaperP>(), 10, true);
             Item.DamageType = ModContent.GetInstance<Bonecursed>();
+            Item.UseSound = SoundID.Item1;
         }
         public override bool MeleePrefix()
         {
