75ac0f2b11b521f7896b5101226603d372653318
ChadBroski01 <aalexx822@gmail.com>
2023-08-31 20:08:55 -0400

    Creates SkeletalReaper and shrinks dust from CartilageSpearProjectile and CartilageSpearShrapnelProjectile, and unfortunately the SkeletalReaper projectile is upside-down when firing to the left

---
+++ Items/Weapons/Calcium/CalcMelee/SkeletalReaper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheSkeletronMod.Common.DamageClasses;
+using TheSkeletronMod.projectiles.Calcprojs.CalcMeleeproj;
+
+namespace TheSkeletronMod.Items.Weapons.Calcium.CalcMelee
+{
+    public class SkeletalReaper : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.ItemDefaultMeleeShootCustomProjectile(54, 33, 130, 7f, 25, 25, ItemUseStyleID.Swing, ModContent.ProjectileType<SkeletalReaperP>(), 10, true);
+            Item.DamageType = ModContent.GetInstance<Bonecursed>();
+        }
+        public override bool MeleePrefix()
+        {
+            return true;
+        }
+    }
+}
---
+++ projectiles/Calcprojs/CalcMeleeproj/SkeletalReaperP.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria;
+using TheSkeletronMod.Common.DamageClasses;
+using System.Security.Cryptography.X509Certificates;
+using Terraria.Utilities;
+
+namespace TheSkeletronMod.projectiles.Calcprojs.CalcMeleeproj
+{
+    public class SkeletalReaperP : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 20;
+            Projectile.height = 200;
+            Projectile.timeLeft = 600;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.penetrate = 2;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.light = 10f;
+            Projectile.DamageType = ModContent.GetInstance<Bonecursed>();
+            Projectile.ArmorPenetration = 2;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            
+            Main.projFrames[Projectile.type] = 7;
+            ProjectileID.Sets.CultistIsResistantTo[Projectile.type] = true;
+        }
+        public bool damageReduced = false;
+        public override void AI()
+        {
+            if (damageReduced == false)
+            {
+                Projectile.damage -= 55;
+                damageReduced = true;
+            }
+            Projectile.rotation = Projectile.velocity.ToRotation();
+            if (++Projectile.frameCounter >= 3f)//the amount of ticks the game spends on each frame
+            {
+                Projectile.frameCounter = 0;
+
+                if (++Projectile.frame >= Main.projFrames[Projectile.type])
+                    Projectile.frame = 0;
+            }
+            //var dust = Dust.NewDustPerfect(Projectile.position + new Vector2(0, 100), DustID.TintableDust, Projectile.velocity/2);
+            //dust.scale = 2.5f;
+            //dust.fadeIn = 0.4f;
+            //dust.noGravity = true;
+            //dust.noLight = false;
+            //dust.color = Color.Purple;
+            //dust.alpha = 0;
+        }
+        //public override void Kill(int timeLeft)
+        //{
+         //   for (int  i = 0;  i < 10;  i++)
+         //   {
+         //       var dust = Dust.NewDustPerfect(Projectile.position + new Vector2(0,100), DustID.TintableDust, new Vector2(-1, -1));
+         //       dust.scale = 2.5f;
+        //        dust.fadeIn = 0.4f;
+        //        dust.noGravity = true;
+        //        dust.noLight = false;
+        //        dust.color = Color.Purple;
+        //        dust.alpha = 0;
+        //    }
+            
+       // }
+    }
+}
