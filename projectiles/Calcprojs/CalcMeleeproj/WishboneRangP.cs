48aee2b96984fc831fdca38334c4ea5d20176b05
ChadBroski01 <aalexx822@gmail.com>
2023-09-02 10:13:04 -0400

    WishboneRang done

--- projectiles/Calcprojs/CalcMeleeproj/WishboneRangP.cs
+++ projectiles/Calcprojs/CalcMeleeproj/WishboneRangP.cs
@@ -25,13 +25,54 @@ namespace TheSkeletronMod.projectiles.Calcprojs.CalcMeleeproj
         }
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
-            Projectile.NewProjectile(Projectile.GetSource_FromThis(),Projectile.position,new Vector2(Projectile.velocity.X * 2, Projectile.velocity.Y), ModContent.ProjectileType<WishboneRangPOne>(), 4, 2, Projectile.owner);
+            Projectile.NewProjectile(Projectile.GetSource_FromThis(),Projectile.position,new Vector2(Projectile.velocity.X * -2, Projectile.velocity.Y), ModContent.ProjectileType<WishboneRangPOne>(), 7, 2, Projectile.owner);
+            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.position, new Vector2(Projectile.velocity.X * -1.6f, Projectile.velocity.Y), ModContent.ProjectileType<WishboneRangPTwo>(), 9, 2, Projectile.owner);
 
         }
 
     }
     public class WishboneRangPOne : ModProjectile
     {
-
+        public override void SetDefaults()
+        {
+            Projectile.aiStyle = 3;
+            Projectile.width = 24;
+            Projectile.height = 24;
+            Projectile.friendly = true;
+            Projectile.scale = 1.2f;
+            Projectile.penetrate = 2;
+            Projectile.DamageType = ModContent.GetInstance<Bonecursed>();
+            Projectile.aiStyle = 1;
+        }
+        //public override void AI()
+        //{
+        //    Projectile.velocity.Y = Projectile.velocity.Y + 0.1f; // 0.1f for arrow gravity, 0.4f for knife gravity
+        //    if (Projectile.velocity.Y > 16f) // terminal velocity
+        //    {
+        //        Projectile.velocity.Y = 16f;
+        //    }
+        //}
+    }
+    public class WishboneRangPTwo : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.aiStyle = 3;
+            Projectile.width = 24;
+            Projectile.height = 24;
+            Projectile.friendly = true;
+            Projectile.scale = 1.2f;
+            Projectile.penetrate = 3;
+            Projectile.DamageType = ModContent.GetInstance<Bonecursed>();
+            Projectile.aiStyle = 1;
+        }
+        //public override void AI()
+        //{
+        //    Projectile.velocity.Y = Projectile.velocity.Y + 0.3f; // 0.1f for arrow gravity, 0.4f for knife gravity
+        //    if (Projectile.velocity.Y > 16f) // terminal velocity
+        //    {
+        //        Projectile.velocity.Y = 16f;
+        //    }
+        //}
     }
 }
