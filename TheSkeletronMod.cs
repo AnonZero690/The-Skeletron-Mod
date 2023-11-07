5afe6bc80c165645f7fe6240b6d10334405cb339
AnonZero960 <NullZero1@outlook.com>
2023-11-07 20:07:11 +0200

    More tile merging stuff

--- TheSkeletronMod.cs
+++ TheSkeletronMod.cs
@@ -15,6 +15,23 @@ namespace TheSkeletronMod
         public static float ModTime { get; internal set; }
         public static object MessageType { get; internal set; }
 
+        public static int[] confectBG = new int[3];
+
+        internal float screenOff;
+
+        //From here
+        private static TileTest v = new();
+        public static TileTest tileMerge => v;
+
+        public class TileTest
+        {
+            public bool this[int tile1, int tile2]
+            {
+                get => Main.tileMerge[tile1][tile2] || Main.tileMerge[tile2][tile1];
+                set => SkeletronModUtils.Merge(tile1, tile2);
+            }
+        }
+        //To here, is stuff Lion8cake allowed me to copy from confection so big hand to him
         internal static object GetLegacySoundSlot(object custom, string v)
         {
             throw new NotImplementedException();
@@ -24,5 +41,7 @@ namespace TheSkeletronMod
         {
             throw new NotImplementedException();
         }
+
+       
     }
 }
