3e21ce41c1504d16585e6d89ef1d366b64deaa4f
AnonZero960 <NullZero1@outlook.com>
2023-11-08 08:50:16 +0200

    fix n bit more

--- NPCs/TownNPCs/DetectiveNPC.cs
+++ NPCs/TownNPCs/DetectiveNPC.cs
@@ -1,6 +1,7 @@
 ﻿using Terraria.ModLoader;
 using Terraria;
 using Terraria.ID;
+using Terraria.GameContent.Personalities;
 
 namespace TheSkeletronMod.NPCs.TownNPCs
 {
@@ -18,6 +19,30 @@ namespace TheSkeletronMod.NPCs.TownNPCs
             NPCID.Sets.AttackTime[Type] = 30;
             NPCID.Sets.AttackAverageChance[Type] = 7;
             NPCID.Sets.HatOffsetY[Type] = 4;
+
+            NPC.Happiness
+                //.SetBiomeAffection<TaintedBiome>(AffectionLevel.Love) This doesnt exist yet.
+                .SetBiomeAffection<SnowBiome>(AffectionLevel.Love)
+                .SetBiomeAffection<ForestBiome>(AffectionLevel.Like)
+                .SetBiomeAffection<UndergroundBiome>(AffectionLevel.Like)
+                .SetBiomeAffection<OceanBiome>(AffectionLevel.Dislike)
+                .SetBiomeAffection<HallowBiome>(AffectionLevel.Dislike)
+                .SetBiomeAffection<DesertBiome>(AffectionLevel.Hate)
+                .SetBiomeAffection<JungleBiome>(AffectionLevel.Hate)
+
+                .SetNPCAffection(NPCID.Steampunker, AffectionLevel.Like)
+                .SetNPCAffection(NPCID.Mechanic, AffectionLevel.Like)
+                .SetNPCAffection(NPCID.ArmsDealer, AffectionLevel.Like)
+                .SetNPCAffection(NPCID.Guide, AffectionLevel.Like)
+                .SetNPCAffection(NPCID.DD2Bartender, AffectionLevel.Like)
+                .SetNPCAffection(NPCID.GoblinTinkerer, AffectionLevel.Like)
+                .SetNPCAffection(NPCID.Clothier, AffectionLevel.Like)
+                .SetNPCAffection(NPCID.TaxCollector, AffectionLevel.Dislike)
+                .SetNPCAffection(NPCID.Truffle, AffectionLevel.Dislike)
+                .SetNPCAffection(NPCID.PartyGirl, AffectionLevel.Dislike)
+                .SetNPCAffection(NPCID.Angler, AffectionLevel.Hate)
+                .SetNPCAffection(NPCID.Pirate, AffectionLevel.Hate)
+            ;
         }
         public override void SetDefaults()
         {
