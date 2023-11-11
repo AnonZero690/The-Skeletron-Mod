94188ccf03541787ec1e829049d85376b3b5cd89
AnonZero960 <NullZero1@outlook.com>
2023-11-11 18:27:45 +0200

    might break

---
+++ Dusts/TaintedSplash.cs
@@ -0,0 +1,12 @@
+﻿using Terraria.ModLoader;
+
+namespace TheSkeletronMod.Dusts
+{
+    public class TaintedSplash : ModDust
+    {
+        public override void SetStaticDefaults()
+        {
+            UpdateType = 110;
+        }
+    }
+}
