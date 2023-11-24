7cbd6958190075116e5c83a79ac3a3e2576d92c5
ChadBroski01 <aalexx822@gmail.com>
2023-11-24 10:29:40 -0500

    fixed the code a bit, but the projectiles are still invisible and I don't know how to fix that

--- Content/Items/Weapons/Calcium/MilkMage/Airstrike.cs
+++ Content/Items/Weapons/Calcium/MilkMage/Airstrike.cs
@@ -49,32 +49,35 @@ namespace TheSkeletronMod.Content.Items.Weapons.Calcium.MilkMage
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            Vector2 target = Main.screenPosition + new Vector2((float)Main.mouseX, (float)Main.mouseY);
+            Vector2 target = Main.screenPosition + new Vector2(Main.mouseX, Main.mouseY);
             float ceilingLimit = target.Y;
             if (ceilingLimit > player.Center.Y - 200f)
             {
                 ceilingLimit = player.Center.Y - 200f;
             }
+            // Loop these functions 3 times.
             for (int i = 0; i < 3; i++)
             {
-                position = player.Center + new Vector2((-(float)Main.rand.Next(0, 401) * player.direction), -600f);
-                position.Y -= (100 * i);
+                position = player.Center - new Vector2(Main.rand.NextFloat(401) * player.direction, 600f);
+                position.Y -= 100 * i;
                 Vector2 heading = target - position;
+
                 if (heading.Y < 0f)
                 {
                     heading.Y *= -1f;
                 }
+
                 if (heading.Y < 20f)
                 {
                     heading.Y = 20f;
                 }
+
                 heading.Normalize();
-                float speedX = heading.X;
-                float speedY = heading.Y + Main.rand.Next(-40, 41) * 0.02f;
-                heading *= new Vector2(speedX, speedY).Length();
-                
-                Projectile.NewProjectile(Item.GetSource_FromAI(), position.X, position.Y, velocity.X, velocity.Y, type, damage * 2, 2, player.whoAmI, 0f, ceilingLimit);
+                heading *= velocity.Length();
+                heading.Y += Main.rand.Next(-40, 41) * 0.02f;
+                Projectile.NewProjectile(source, position, heading, type, damage * 2, knockback, player.whoAmI, 0f, ceilingLimit);
             }
+
             return false;
         }
     }
