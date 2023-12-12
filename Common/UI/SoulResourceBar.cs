d47803305e3b3aca24911a3af773f96818575c10
Xinim <LELwhyyoulookingatthis@gmail.com>
2023-12-12 20:35:56 +0700

    fuck yo UI

--- Common/UI/SoulResourceBar.cs
+++ Common/UI/SoulResourceBar.cs
@@ -1,204 +1,204 @@
-﻿using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework;
-using System.Collections.Generic;
-using Terraria.GameContent.UI.Elements;
-using Terraria.GameContent;
-using Terraria.Localization;
-using Terraria.ModLoader;
-using Terraria.UI;
-using Terraria;
-using TheSkeletronMod.Common.Systems;
+﻿//using Microsoft.Xna.Framework.Graphics;
+//using Microsoft.Xna.Framework;
+//using System.Collections.Generic;
+//using Terraria.GameContent.UI.Elements;
+//using Terraria.GameContent;
+//using Terraria.Localization;
+//using Terraria.ModLoader;
+//using Terraria.UI;
+//using Terraria;
+//using TheSkeletronMod.Common.Systems;
 
-namespace TheSkeletronMod.Common.UI
-{
-    // This custom UI will show whenever the player is holding the ExampleCustomResourceWeapon item and will display the player's custom resource amounts that are tracked in ExampleResourcePlayer
-    internal class SoulResourceBar : UIState
-    {
-        // For this bar we'll be using a frame texture and then a gradient inside bar, as it's one of the more simpler approaches while still looking decent.
-        // Once this is all set up make sure to go and do the required stuff for most UI's in the ModSystem class.
+//namespace TheSkeletronMod.Common.UI
+//{
+//    // This custom UI will show whenever the player is holding the ExampleCustomResourceWeapon item and will display the player's custom resource amounts that are tracked in ExampleResourcePlayer
+//    internal class SoulResourceBar : UIState
+//    {
+//        // For this bar we'll be using a frame texture and then a gradient inside bar, as it's one of the more simpler approaches while still looking decent.
+//        // Once this is all set up make sure to go and do the required stuff for most UI's in the ModSystem class.
 
-        //private UIText text;
-        private UIElement area;
-        private UIImage barFrame;
-        private Color gradientA;
-        private Color gradientB;
-        int frame = 0;
-        int framecount;
+//        //private UIText text;
+//        private UIElement area;
+//        private UIImage barFrame;
+//        private Color gradientA;
+//        private Color gradientB;
+//        int frame = 0;
+//        int framecount;
 
-        public override void OnInitialize()
-        {
-            // Create a UIElement for all the elements to sit on top of, this simplifies the numbers as nested elements can be positioned relative to the top left corner of this element. 
-            // UIElement is invisible and has no padding.
-            area = new UIElement();
-            area.Left.Set(-area.Width.Pixels - 600, 1f); // Place the resource bar to the left of the hearts.         //done
-            area.Top.Set(20, 0f); // Placing it just a bit below the top of the screen.                          //done
-            area.Width.Set(90, 0f); // We will be placing the following 2 UIElements within this 182x60 area.
-            area.Height.Set(60, 0f);
+//        public override void OnInitialize()
+//        {
+//            // Create a UIElement for all the elements to sit on top of, this simplifies the numbers as nested elements can be positioned relative to the top left corner of this element. 
+//            // UIElement is invisible and has no padding.
+//            area = new UIElement();
+//            area.Left.Set(-area.Width.Pixels - 600, 1f); // Place the resource bar to the left of the hearts.         //done
+//            area.Top.Set(20, 0f); // Placing it just a bit below the top of the screen.                          //done
+//            area.Width.Set(90, 0f); // We will be placing the following 2 UIElements within this 182x60 area.
+//            area.Height.Set(60, 0f);
 
-            barFrame = new UIImage(ModContent.Request<Texture2D>("TheSkeletronMod/Common/UI/SoulResourceFrame"));
+//            barFrame = new UIImage(ModContent.Request<Texture2D>("TheSkeletronMod/Common/UI/SoulResourceFrame"));
 
 
-            //barFrame = new UIAnimatedImage { Width = area.Width, Height = area.Height };
+//            //barFrame = new UIAnimatedImage { Width = area.Width, Height = area.Height };
             
-            // Frame of our resource bar
-            barFrame.Left.Set(22, 0f);
-            barFrame.Top.Set(0, 0f);       //height on the screen (both assests)
-            barFrame.Width.Set(13, 20);
-            barFrame.Height.Set(30, 0f);    //height of resource bar
+//            // Frame of our resource bar
+//            barFrame.Left.Set(22, 0f);
+//            barFrame.Top.Set(0, 0f);       //height on the screen (both assests)
+//            barFrame.Width.Set(13, 20);
+//            barFrame.Height.Set(30, 0f);    //height of resource bar
 
-            //text = new UIText("0/0", 0.8f); // text to show stat
-            //text.Width.Set(138, 0f);
-            //text.Height.Set(34, 0f);
-            //text.Top.Set(70, 0f);
-            //text.Left.Set(0, 0f);
+//            //text = new UIText("0/0", 0.8f); // text to show stat
+//            //text.Width.Set(138, 0f);
+//            //text.Height.Set(34, 0f);
+//            //text.Top.Set(70, 0f);
+//            //text.Left.Set(0, 0f);
 
-            gradientA = new Color(56, 255, 255); // A dark cyan
-            gradientB = new Color(206, 254, 255); // A light purple
+//            gradientA = new Color(56, 255, 255); // A dark cyan
+//            gradientB = new Color(206, 254, 255); // A light purple
 
-            //area.Append(text);
-            area.Append(barFrame);
-            Append(area);
-        }
+//            //area.Append(text);
+//            area.Append(barFrame);
+//            Append(area);
+//        }
 
-        public override void Draw(SpriteBatch spriteBatch)
-        {
-            // This prevents drawing unless we are using an ExampleCustomResourceWeapon
-            //if (Main.LocalPlayer.HeldItem.ModItem is not MarrowMelter)
-                //return;
+//        public override void Draw(SpriteBatch spriteBatch)
+//        {
+//            // This prevents drawing unless we are using an ExampleCustomResourceWeapon
+//            //if (Main.LocalPlayer.HeldItem.ModItem is not MarrowMelter)
+//                //return;
 
-            base.Draw(spriteBatch);
-        }
+//            base.Draw(spriteBatch);
+//        }
 
-        public override void Update(GameTime gameTime)
-        {
-            if (framecount > 0)
-            {
-                framecount--;
-            }
+//        public override void Update(GameTime gameTime)
+//        {
+//            if (framecount > 0)
+//            {
+//                framecount--;
+//            }
 
-            if (framecount == 0)
-            {
-                if (frame < 105)
-                {
-                    frame += 84;
-                }
-                else
-                {
-                    frame = 0;
-                }
+//            if (framecount == 0)
+//            {
+//                if (frame < 105)
+//                {
+//                    frame += 84;
+//                }
+//                else
+//                {
+//                    frame = 0;
+//                }
 
-                framecount = 8;
-            }
-
-
-
-            base.Update(gameTime);
-        }
+//                framecount = 8;
+//            }
 
 
-        // Here we draw our UI
-        protected override void DrawSelf(SpriteBatch spriteBatch)
-        { 
 
-            int frame = 0;
-            int framecount;
-
-            const int X = 958;
-            const int Y = 530;
-
-            Vector2 locc = new Vector2(X, Y);
-
-            Texture2D texture = (Texture2D)ModContent.Request<Texture2D>("TheSkeletronMod/Common/UI/SoulResourceFrame");
-
-            Rectangle sourceRect = new Rectangle(0, frame, texture.Width, 136);
-
-            Main.EntitySpriteDraw(texture, locc, sourceRect, Color.White, 0f, sourceRect.Size() / 2f, 1f, SpriteEffects.None, 0);
+//            base.Update(gameTime);
+//        }
 
 
+//        // Here we draw our UI
+//        protected override void DrawSelf(SpriteBatch spriteBatch)
+//        { 
+
+//            int frame = 0;
+//            int framecount;
+
+//            const int X = 958;
+//            const int Y = 530;
+
+//            Vector2 locc = new Vector2(X, Y);
+
+//            Texture2D texture = (Texture2D)ModContent.Request<Texture2D>("TheSkeletronMod/Common/UI/SoulResourceFrame");
+
+//            Rectangle sourceRect = new Rectangle(0, frame, texture.Width, 136);
+
+//            Main.EntitySpriteDraw(texture, locc, sourceRect, Color.White, 0f, sourceRect.Size() / 2f, 1f, SpriteEffects.None, 0);
 
 
 
 
-            base.DrawSelf(spriteBatch);
 
-            var modPlayer = Main.LocalPlayer.GetModPlayer<SoulResourcePlayer>();
-            // Calculate quotient
 
-            float quotient = (float)modPlayer.SoulResourceCurrent / modPlayer.SoulResourceMax2; // Creating a quotient that represents the difference of your currentResource vs your maximumResource, resulting in a float of 0-1f.
-            quotient = MathHelper.Clamp(quotient, 0f, 1f); // Clamping it to 0-1f so it doesn't go over that.
+//            base.DrawSelf(spriteBatch);
 
-            // Here we get the screen dimensions of the barFrame element, then tweak the resulting rectangle to arrive at a rectangle within the barFrame texture that we will draw the gradient. These values were measured in a drawing program.
-            Rectangle hitbox = barFrame.GetInnerDimensions().ToRectangle();
-            hitbox.X += 18;
-            hitbox.Width -= 24;
-            hitbox.Y += 30;
-            hitbox.Height -= 16;
+//            var modPlayer = Main.LocalPlayer.GetModPlayer<SoulResourcePlayer>();
+//            // Calculate quotient
 
-            // Now, using this hitbox, we draw a gradient by drawing vertical lines while slowly interpolating between the 2 colors.
-            int left = hitbox.Left;
-            int right = hitbox.Right;
-            int steps = (int)((right - left) * quotient);
-            for (int i = 0; i < steps; i += 1)
-            {
-                // float percent = (float)i / steps; // Alternate Gradient Approach
-                float percent = (float)i / (right - left);
-                spriteBatch.Draw(TextureAssets.MagicPixel.Value, new Rectangle(left + i, hitbox.Y, 1, hitbox.Height), Color.Lerp(gradientA, gradientB, percent));
-            }
-        }
+//            float quotient = (float)modPlayer.SoulResourceCurrent / modPlayer.SoulResourceMax2; // Creating a quotient that represents the difference of your currentResource vs your maximumResource, resulting in a float of 0-1f.
+//            quotient = MathHelper.Clamp(quotient, 0f, 1f); // Clamping it to 0-1f so it doesn't go over that.
 
-        //public override void Update(GameTime gameTime)
-        //{
-            //if (Main.LocalPlayer.HeldItem.ModItem is not MarrowMelter)
-                //return;
+//            // Here we get the screen dimensions of the barFrame element, then tweak the resulting rectangle to arrive at a rectangle within the barFrame texture that we will draw the gradient. These values were measured in a drawing program.
+//            Rectangle hitbox = barFrame.GetInnerDimensions().ToRectangle();
+//            hitbox.X += 18;
+//            hitbox.Width -= 24;
+//            hitbox.Y += 30;
+//            hitbox.Height -= 16;
 
-            //var modPlayer = Main.LocalPlayer.GetModPlayer<SoulResourcePlayer>();
-            // Setting the text per tick to update and show our resource values.
-            //text.SetText(SoulResourceUISystem.SoulResourceText.Format(modPlayer.SoulResourceCurrent, modPlayer.SoulResourceMax2));
-            //base.Update(gameTime);
-        //}
-    }
+//            // Now, using this hitbox, we draw a gradient by drawing vertical lines while slowly interpolating between the 2 colors.
+//            int left = hitbox.Left;
+//            int right = hitbox.Right;
+//            int steps = (int)((right - left) * quotient);
+//            for (int i = 0; i < steps; i += 1)
+//            {
+//                // float percent = (float)i / steps; // Alternate Gradient Approach
+//                float percent = (float)i / (right - left);
+//                spriteBatch.Draw(TextureAssets.MagicPixel.Value, new Rectangle(left + i, hitbox.Y, 1, hitbox.Height), Color.Lerp(gradientA, gradientB, percent));
+//            }
+//        }
 
-    // This class will only be autoloaded/registered if we're not loading on a server
-    [Autoload(Side = ModSide.Client)]
-    internal class SoulResourceUISystem : ModSystem
-    {
-        private UserInterface SoulResourceBarUserInterface;
+//        //public override void Update(GameTime gameTime)
+//        //{
+//            //if (Main.LocalPlayer.HeldItem.ModItem is not MarrowMelter)
+//                //return;
 
-        internal SoulResourceBar SoulResourceBar;
+//            //var modPlayer = Main.LocalPlayer.GetModPlayer<SoulResourcePlayer>();
+//            // Setting the text per tick to update and show our resource values.
+//            //text.SetText(SoulResourceUISystem.SoulResourceText.Format(modPlayer.SoulResourceCurrent, modPlayer.SoulResourceMax2));
+//            //base.Update(gameTime);
+//        //}
+//    }
 
-        public static LocalizedText SoulResourceText { get; private set; }
+//    // This class will only be autoloaded/registered if we're not loading on a server
+//    [Autoload(Side = ModSide.Client)]
+//    internal class SoulResourceUISystem : ModSystem
+//    {
+//        private UserInterface SoulResourceBarUserInterface;
 
-        public override void Load()
-        {
-            SoulResourceBar = new();
-            SoulResourceBarUserInterface = new();
-            SoulResourceBarUserInterface.SetState(SoulResourceBar);
+//        internal SoulResourceBar SoulResourceBar;
 
-            string category = "UI";
-            SoulResourceText ??= Mod.GetLocalization($"{category}.SoulResource");
-        }
+//        public static LocalizedText SoulResourceText { get; private set; }
 
-        public override void UpdateUI(GameTime gameTime)
-        {
-            SoulResourceBarUserInterface?.Update(gameTime);
-        }
+//        public override void Load()
+//        {
+//            SoulResourceBar = new();
+//            SoulResourceBarUserInterface = new();
+//            SoulResourceBarUserInterface.SetState(SoulResourceBar);
 
-        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
-        {
-            int resourceBarIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Resource Bars"));
-            if (resourceBarIndex != -1)
-            {
-                layers.Insert(resourceBarIndex, new LegacyGameInterfaceLayer(
-                    "Soul Bar: Soul Resource Bar",
-                    delegate {
-                        SoulResourceBarUserInterface.Draw(Main.spriteBatch, new GameTime());
-                        return true;
-                    },
-                    InterfaceScaleType.UI)
-                );
-            }
-        }
+//            string category = "UI";
+//            SoulResourceText ??= Mod.GetLocalization($"{category}.SoulResource");
+//        }
 
-    }
+//        public override void UpdateUI(GameTime gameTime)
+//        {
+//            SoulResourceBarUserInterface?.Update(gameTime);
+//        }
 
-}
+//        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+//        {
+//            int resourceBarIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Resource Bars"));
+//            if (resourceBarIndex != -1)
+//            {
+//                layers.Insert(resourceBarIndex, new LegacyGameInterfaceLayer(
+//                    "Soul Bar: Soul Resource Bar",
+//                    delegate {
+//                        SoulResourceBarUserInterface.Draw(Main.spriteBatch, new GameTime());
+//                        return true;
+//                    },
+//                    InterfaceScaleType.UI)
+//                );
+//            }
+//        }
+
+//    }
+
+//}
